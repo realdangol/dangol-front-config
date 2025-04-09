95a263329040e7f37f4a2ea4789e275f85dfa5ae
HyeBinYang <skdisk7368@gmail.com>
2025-04-10 01:35:29 +0900

    feat: config 업데이트

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,18 +1,37 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "import", "react", "react-hooks"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "prettier",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended", // prettier와 충돌 방지
   ],
+  plugins: ["@typescript-eslint", "react", "react-hooks", "import", "simple-import-sort"],
   rules: {
-    semi: ["error", "always"],
-    quotes: ["error", "single"],
+    // 포맷팅 관련은 Prettier에 맡기고, 로직 중심 룰만 넣기
+    "react/react-in-jsx-scope": "off", // Next.js에서는 필요 없음
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "import/order": "off",
+    "simple-import-sort/imports": "warn",
+    "simple-import-sort/exports": "warn",
   },
   settings: {
     react: {
--- packages/prettier/prettier.config.js
+++ packages/prettier/prettier.config.js
@@ -1,7 +1,12 @@
+/** @type {import("prettier").Config} */
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  trailingComma: "all",
-  printWidth: 100,
-  tabWidth: 2,
+  semi: true, // 줄 끝에 세미콜론 붙이기
+  singleQuote: true, // 작은따옴표 사용
+  trailingComma: 'all', // 마지막 항목에도 쉼표 붙이기 (멀티라인에서 유리)
+  printWidth: 100, // 한 줄 최대 길이 (보통 100~120)
+  tabWidth: 2, // 탭 간격
+  useTabs: false, // 공백 2칸 대신 탭을 쓸지 여부
+  bracketSpacing: true, // 객체 리터럴 중괄호 양옆에 공백
+  arrowParens: 'avoid', // (x) => x 대신 x => x
+  endOfLine: 'auto', // OS별 줄바꿈 스타일 자동 감지 (CRLF/LF 충돌 방지)
 };
