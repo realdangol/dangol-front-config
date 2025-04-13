31c08e7c4192568121aa80b87a6782c3efad1a82
HyeBinYang <skdisk7368@gmail.com>
2025-04-13 19:57:34 +0900

    feat: eslint react 수정

--- packages/eslint/utils/react.js
+++ packages/eslint/utils/react.js
@@ -1,8 +1,7 @@
 module.exports = {
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
+  plugins: ["react", "react-hooks"],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-    "react/react-in-jsx-scope": "off",
   },
 };
